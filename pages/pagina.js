d81f685bc1e4d3f194b1f42f288b3091fd1c5df3
Axel <axosar@gmail.com>
2021-04-27 17:35:32 -0500

    Botones

--- pages/pagina.js
+++ pages/pagina.js
@@ -19,15 +19,31 @@ export default function Pagina() {
             <img src="guerita.png" className="img-fluid" alt="" title="" />
           </Col>
           <Col className="bg-light">
-          <div className="tarjetax p-3">
-            <h3>Lorem ipsum dolor</h3>
-            <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, dui vitae luctus tristique, magna lectus maximus felis, nec tincidunt mi nunc ullamcorper lacus. Morbi quis enim bibendum, eleifend erat eget, luctus leo. Donec hendrerit placerat massa, eu pellentesque metus euismod eu. Mauris a sapien id tortor facilisis tempus. Pellentesque at ipsum scelerisque, tincidunt odio vitae, finibus sapien. Vivamus varius scelerisque arcu. Vivamus auctor nunc vitae diam sollicitudin sodales. Morbi tristique lorem id risus consequat, quis pellentesque dolor imperdiet. Praesent libero metus, dapibus ut laoreet vel, efficitur vel lectus. Nullam condimentum et nisi ut venenatis. Proin mollis sem ut dui vehicula, non sagittis ipsum tincidunt. Curabitur malesuada rhoncus orci in sodales. Suspendisse sit amet lorem nisi. Maecenas elementum nulla nec iaculis faucibus. Cras ultrices eros ut enim scelerisque, eu blandit tortor pellentesque. Maecenas eget dui non est egestas vehicula.</small></p>
-          </div>
+            <div className="tarjetax p-3">
+              <h3>Lorem ipsum dolor</h3>
+              <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, dui vitae luctus tristique, magna lectus maximus felis, nec tincidunt mi nunc ullamcorper lacus. Morbi quis enim bibendum, eleifend erat eget, luctus leo. Donec hendrerit placerat massa, eu pellentesque metus euismod eu. Mauris a sapien id tortor facilisis tempus. Pellentesque at ipsum scelerisque, tincidunt odio vitae, finibus sapien. Vivamus varius scelerisque arcu. Vivamus auctor nunc vitae diam sollicitudin sodales. Morbi tristique lorem id risus consequat, quis pellentesque dolor imperdiet. Praesent libero metus, dapibus ut laoreet vel, efficitur vel lectus. Nullam condimentum et nisi ut venenatis. Proin mollis sem ut dui vehicula, non sagittis ipsum tincidunt. Curabitur malesuada rhoncus orci in sodales. Suspendisse sit amet lorem nisi. Maecenas elementum nulla nec iaculis faucibus. Cras ultrices eros ut enim scelerisque, eu blandit tortor pellentesque. Maecenas eget dui non est egestas vehicula.</small></p>
+            </div>
           </Col>
-          
         </Row>
-        <Row className="p-3 bg-light">
-          
+        <Row className="mt-5">
+          <Col md={4} className="d-flex justify-content-center align-items-center">
+            <h3>Lorem ipsum dolor</h3>
+          </Col>
+          <Col className="bg-light">
+            <div className="tarjetax p-3">
+              <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, dui vitae luctus tristique, magna lectus maximus felis, nec tincidunt mi nunc ullamcorper lacus. Morbi quis enim bibendum, eleifend erat eget, luctus leo. Donec hendrerit placerat massa, eu pellentesque metus euismod eu. Mauris a sapien id tortor facilisis tempus. Pellentesque at ipsum scelerisque, tincidunt odio vitae, finibus sapien. Vivamus varius scelerisque arcu. Vivamus auctor nunc vitae diam sollicitudin sodales. Morbi tristique lorem id risus consequat, quis pellentesque dolor imperdiet. Praesent libero metus, dapibus ut laoreet vel, efficitur vel lectus. Nullam condimentum et nisi ut venenatis. Proin mollis sem ut dui vehicula, non sagittis ipsum tincidunt. Curabitur malesuada rhoncus orci in sodales. Suspendisse sit amet lorem nisi. Maecenas elementum nulla nec iaculis faucibus. Cras ultrices eros ut enim scelerisque, eu blandit tortor pellentesque. Maecenas eget dui non est egestas vehicula.</small></p>
+            </div>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col md={4} className="d-flex justify-content-center align-items-center">
+            <h3>Lorem ipsum dolor</h3>
+          </Col>
+          <Col className="bg-light">
+            <div className="tarjetax p-3">
+              <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, dui vitae luctus tristique, magna lectus maximus felis, nec tincidunt mi nunc ullamcorper lacus. Morbi quis enim bibendum, eleifend erat eget, luctus leo. Donec hendrerit placerat massa, eu pellentesque metus euismod eu. Mauris a sapien id tortor facilisis tempus. Pellentesque at ipsum scelerisque, tincidunt odio vitae, finibus sapien. Vivamus varius scelerisque arcu. Vivamus auctor nunc vitae diam sollicitudin sodales. Morbi tristique lorem id risus consequat, quis pellentesque dolor imperdiet. Praesent libero metus, dapibus ut laoreet vel, efficitur vel lectus. Nullam condimentum et nisi ut venenatis. Proin mollis sem ut dui vehicula, non sagittis ipsum tincidunt. Curabitur malesuada rhoncus orci in sodales. Suspendisse sit amet lorem nisi. Maecenas elementum nulla nec iaculis faucibus. Cras ultrices eros ut enim scelerisque, eu blandit tortor pellentesque. Maecenas eget dui non est egestas vehicula.</small></p>
+            </div>
+          </Col>
         </Row>
       </Container>
     </Layout>
