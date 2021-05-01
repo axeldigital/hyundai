474a75e26712e85d97747d50b8af3ee74a9275d3
Axel <axosar@gmail.com>
2021-05-01 13:38:44 -0500

    rutas

--- pages/modelo/[id].js
+++ pages/modelo/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout';
+import Layout from '../../components/layout';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
