e82a79ee82b3999c2f13d0179d968088edb15556
Axel <axosar@gmail.com>
2021-05-17 18:09:48 -0500

    colores

--- pages/programa.js
+++ pages/programa.js
@@ -22,23 +22,23 @@ export default function Programa() {
   return (
     <Layout>
       <Slider className="mt-3" imagen="hero6.png" texto="¿Qué es Hyundai Promise?" />
-      <Container>
+      <Container style={{marginBottom: '50px'}}>
         <Row>
           <Col md={4}>
             <img src="programa1.png" className="img-fluid" alt="" title="" />
           </Col>
-          <Col className="bg-light d-flex align-items-center">
+          <Col className="bg-light d-flex align-items-center" style={{paddingBottom: '100px'}}>
             <div className="tarjetax p-3">
               <p><small>Hyundai de Mexico es el programa de autos seminuevos certificados más enfocado en la calidad y servicio, con los mejores beneficios del mercado. </small></p>
-              <h3>Características</h3>
+              <h3 style={{color: "#012E5E"}}>Características</h3>
               <p><small>Para garantizar que los vehículos alcancen el máximo nivel de calidad y seguridad, todos nuestros autos seminuevos han pasado por una exhaustiva revisión de 162 puntos para obtener su certificación, de esta manera tu auto estará en las mejores condiciones de acuerdo a su edad y modelo. Además de contar con beneficios exclusivos como:</small></p>
-              <h3>Hyundai Finnance</h3>
+              <h3 style={{color: "#012E5E"}}>Hyundai Finnance</h3>
               <p><small>Solo por ser parte del programa de Hyundai Promise ofrecemos productos financieros con beneficios especiales para Seminuevos Certificados.</small></p>
-              <h3>Hyundai Protect</h3>
+              <h3 style={{color: "#012E5E"}}>Hyundai Protect</h3>
               <p><small>Como parte de los beneficios de Hyundai Promise ofrecemos seguros con coberturas especiales para Seminuevos Certificados.</small></p>
-              <h3>ASSURANT</h3>
+              <h3 style={{color: "#012E5E"}}>ASSURANT</h3>
               <p><small>Líder en el segmento de Seminuevos Certificados.</small></p>
-              <h3>TOMA Y VENTA</h3>
+              <h3 style={{color: "#012E5E"}}>TOMA Y VENTA</h3>
               <p><small>Transparencia y confianza en todo el proceso.</small></p>
             </div>
           </Col>
